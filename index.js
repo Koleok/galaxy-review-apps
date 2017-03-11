4a96e38783835f2a47832513eb8085cac94f1f83
Koleok <kchamb3@gmail.com>
2017-03-11 15:53:14 -0500

    Add enough for first use case

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const { resolve } = require('path')
+const { branch } = require('git-rev')
+const { cyan, gray, red, bold } = require('chalk')
+
+const { name: moduleName } = require(resolve(__dirname, 'package.json'))
+const limit = 63
+
+branch(branchName => {
+  const url = `${moduleName}-${branchName}.meteorapp.com`
+  const willDeploy = url.length <= limit
+
+  const message = willDeploy
+    ? cyan(`😸  ${bold(url)} will live a happy life as a review app`)
+    : red(`😱  ${bold(url)} will probably fail to deploy as a review app because its name is over the galaxy limit of ${bold(limit)} characters`)
+
+  const logMethod = willDeploy ? 'info' : 'error'
+  console[logMethod](gray(`
+    *** Galaxy review apps ***
+
+    ${message}
+  `))
+
+  process.exit()
+})
